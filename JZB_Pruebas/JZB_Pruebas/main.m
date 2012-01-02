d505f845052db75eccfc6bf96c2d65f19d355979
jzarzuela <jzarzuela@gmail.com>
2012-01-02 16:56:28 +0100

    setup in mac

--- JZB_Pruebas/JZB_Pruebas/main.m
+++ JZB_Pruebas/JZB_Pruebas/main.m
@@ -14,7 +14,7 @@ int main (int argc, const char * argv[])
     NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
 
     // insert code here...
-    NSLog(@"Hello, World 2!");
+    NSLog(@"Hello, World!");
 
     [pool drain];
     return 0;
