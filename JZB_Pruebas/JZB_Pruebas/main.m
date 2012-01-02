2e73df963ce91d5459b28441c3ce708dbf5875e7
jzarzuela <jzarzuela@gmail.com>
2012-01-02 16:42:27 +0100

    Una prueba

--- JZB_Pruebas/JZB_Pruebas/main.m
+++ JZB_Pruebas/JZB_Pruebas/main.m
@@ -14,7 +14,7 @@ int main (int argc, const char * argv[])
     NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
 
     // insert code here...
-    NSLog(@"Hello, World!");
+    NSLog(@"Hello, World 2!");
 
     [pool drain];
     return 0;
