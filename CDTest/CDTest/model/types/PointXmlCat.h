2b0f01e51021ca80597a8d40b3af4c274517c15e
jzarzuela <jzarzuela@gmail.com>
2012-02-06 02:19:40 +0100

    cambios varios

---
+++ CDTest/CDTest/model/types/PointXmlCat.h
@@ -0,0 +1,16 @@
+//
+//  PointXmlUtil.h
+//  CDTest
+//
+//  Created by jzarzuela on 06/02/12.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface PointXmlUtil : NSObject {
+    
+}
+
+@end
---
+++ CDTest/CDTest/model/types/PointXmlCat.m
@@ -0,0 +1,14 @@
+//
+//  PointXmlUtil.m
+//  CDTest
+//
+//  Created by jzarzuela on 06/02/12.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "PointXmlUtil.h"
+
+
+@implementation PointXmlUtil
+
+@end
