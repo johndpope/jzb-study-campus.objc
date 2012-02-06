2222054037a0ba48a3ffbe214a32349809304189
jzarzuela <jzarzuela@gmail.com>
2012-02-07 00:24:39 +0100

    cambios varios

---
+++ CDTest/CDTest/services/MergeEntityCat.h
@@ -0,0 +1,14 @@
+//
+//  MergeEntityCat.h
+//  CDTest
+//
+//  Created by jzarzuela on 06/02/12.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface TPoint (MergeEntityCat)
+
+@end
---
+++ CDTest/CDTest/services/MergeEntityCat.m
@@ -0,0 +1,14 @@
+//
+//  MergeEntityCat.m
+//  CDTest
+//
+//  Created by jzarzuela on 06/02/12.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "MergeEntityCat.h"
+
+
+@implementation TPoint (MergeEntityCat)
+
+@end
